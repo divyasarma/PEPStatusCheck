8435f400d71e1820c65857f139f6f2dbc9a9f299
Divya Sarma <rdivyasarma@gmail.com>
2021-11-23 14:47:32 +0100

    Modified browser list and added path import

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const SampleData1API = require( "./DataExtraction/SampleInvoke");
 const http = require("http");
 const express = require("express");
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
