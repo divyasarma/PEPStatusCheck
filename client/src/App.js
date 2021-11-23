b23f72a55a32908f4cf07bd9bb7e5f1f13d5084f
Divya Sarma <rdivyasarma@gmail.com>
2021-11-23 21:39:51 +0100

    Added styling changes and loading state in ui while waiting for response

--- client/src/App.js
+++ client/src/App.js
@@ -8,22 +8,27 @@ function App() {
 
   const [data, setData] = React.useState(null);
   const [input,setInput] =React.useState('');
-
+  const [isComplete,setComplete] = React.useState('');
+  const state = {
+    DONE : 'DONE',
+    LOADING : 'LOADING'
+  }
 
   const callData =()=>{
-    console.log("called calldata ?",input)
+    setComplete(state.LOADING);
     axios.get("/pepStatus?name="+input)
     .then((res) => {
       if(res.data){
-        console.log(res.data)
         setData(res.data);
+        setComplete(state.DONE);
       } 
     })
     .catch((res)=>{
+      setComplete(state.DONE);
       console.log("error",res)
     });
   }
-  const DisplayData= !data ? "" : (data.message ? "Person not found!" :data.map(
+  const DisplayData= data && !data.message && data.map(
     (info)=>{
         return(
             <tr>
@@ -35,7 +40,35 @@ function App() {
         )
     }
 )
-  )
+  
+  const Display = ({data}) => {
+    if(isComplete === state.LOADING){
+      return "Loading..."
+    }
+    if(isComplete === state.DONE){
+      if(data && data.message){
+        return data.message
+      }
+      else{
+        return (<table id="customers">
+              <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Birth Date</th>
+                    <th>PEP Status</th>
+                    <th>PEP Score</th>
+                  </tr>
+          </thead>
+          <tbody>        
+            {DisplayData}        
+          </tbody>
+        </table>
+        )
+      }
+    } 
+     else
+      return null      
+  } 
   return (
     <div className="App">
       <header className="App-header">
@@ -48,25 +81,9 @@ function App() {
     <div><input className="textbox" type="text" value={input} onChange={e => setInput(e.target.value)} onKeyPress={e => {if(e.key === 'Enter'){callData();}}}/>
         <button className="send"onClick={callData}>Send</button>
     </div>
-    {data ? <div>
-             <table id="customers">
-                <thead>
-                    <tr>
-                    <th>Name</th>
-                    <th>Birth Date</th>
-                    <th>PEP Status</th>
-                    <th>PEP Score</th>
-                    </tr>
-                </thead>
-                <tbody>
-                 
-                    
-                    {DisplayData}
-                    
-                </tbody>
-            </table>
-             
-        </div> :null} 
+    <div>
+          <Display data={data}/>
+    </div>
     </div>
   );
 }
